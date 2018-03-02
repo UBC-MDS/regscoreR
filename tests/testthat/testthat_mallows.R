3c828559d3877f92070a98251da0076d60ed2a04
RUOQI XU <7Ymr@dhcp-128-189-236-57.ubcsecure.wireless.ubc.ca>
2018-03-01 16:37:28 -0800

    Edit test for mallows

--- tests/testthat/testthat_mallows.R
+++ tests/testthat/testthat_mallows.R
@@ -1,1 +1,18 @@
-# add tests for mallows cp
+#library(testthat)
+#library(regscoreR)
+
+context("Testing mallows")
+
+# mallows
+test_that("mallows() returns Mallow's Cp of the model", {
+  # expected outputs:
+  expect_equal(round(aic(y,y_pred,4),5), value)
+
+
+  # expected errors:
+  expect_error(aic([1,2,3],[2,2,3],-1))
+  expect_error(aic(y, y_pred, 3.3))
+  expect_error(aic("a", [[1,2],[2,2]], 3))
+  expect_error(aic([1,2], [1,2,3],2))
+  expect_error(aic([1], [3],1))
+})
