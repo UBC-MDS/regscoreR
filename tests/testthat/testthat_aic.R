4f87b8788ca60ce5bbe8ad5734a7093460867ba7
Ha Dinh <dinhhn.ubc@gmail.com>
2018-03-03 13:43:14 -0800

    Added more tests Include different type tests for y and y_pred, value tests for p etc.

--- tests/testthat/testthat_aic.R
+++ tests/testthat/testthat_aic.R
@@ -15,19 +15,32 @@ test_that("aic() returns an aic score of a specific model, given observed y, pre
 
   ## test if length of y is larger than 1, if not yield error
   expect_error(aic(c(), c(5,6,7,8), 3), "y should have length > 1 with equal length to y_pred")
+  expect_error(aic(c(2), c(5,6,7,8), 3), "y should have length > 1 with equal length to y_pred")
 
   ## test if length of y_pred is larger than 1, if not yield error
   expect_error(aic(c(1,2,3,4), c(3), 3), "y_pred should have length > 1 with equal length to y")
+  expect_error(aic(c(1,2,3,4), c(), 3), "y_pred should have length > 1 with equal length to y")
+
+  ## test if y is a vector or array-like type including numbers, if not yield error
+  expect_error(aic(as.data.frame(c(1,2,3,4)), c(5,6,7,8), 3), "y should be vector")
+  expect_error(aic(2, c(5,6,7,8), 3), "y should be vector")
+  expect_error(aic(c("a","b","c","d"), c(5,6,7,8), 3), "y should be vector")
+  expect_error(aic("a", c(5,6,7,8), 3), "y should be vector")
+
+  ## test if y_pred is a vector or array-like type including numbers, if not yield error
+  expect_error(aic(c(1,2,3,4), as.data.frame(c(5,6,7,8)), 3), "y_pred should be vector")
+  expect_error(aic(c(1,2,3,4), 2, 3), "y_pred should be vector")
+  expect_error(aic(c(1,2,3,4), c("a","b","c","d"), 3), "y_pred should be vector")
+  expect_error(aic(c(1,2,3,4), "a", 3), "y_pred should be vector")
 
   ## test if p is int, if not yield error
   expect_error(aic(c(1,2,3,4), c(5,6,7,8), "a"), "non-numeric argument")
-
-  ## test if y is a vector or array-like type, if not yield error
-  expect_error(aic(as.data.frame(c(1,2,3,4)), c(5,6,7,8), 3), "y should be vector")
-
-  ## test if y_pred is a vector or array-like type, if not yield error
-  expect_error(aic(c(1,2,3,4), as.data.frame(c(5,6,7,8)), 3), "y_pred should be vector")
+  expect_error(aic(c(1,2,3,4), c(5,6,7,8), c(2,0,0,0)), "non-numeric argument")
+  expect_error(aic(c(1,2,3,4), c(5,6,7,8), 4.75), "non-numeric argument")
 
   ## test if p is larger than 0, if not yield error
   expect_error(aic(c(1,2,3,4), c(5,6,7,8), 0, "p should be positive"))
+  expect_error(aic(c(1,2,3,4), c(5,6,7,8), -5, "p should be positive"))
 })
+
+
