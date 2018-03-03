fd131de69001e9bfe9a09f14b137dbd82908fae8
Ha Dinh <dinhhn.ubc@gmail.com>
2018-03-03 12:25:11 -0800

    Added more test cases for AIC

--- tests/testthat/testthat_aic.R
+++ tests/testthat/testthat_aic.R
@@ -3,9 +3,11 @@
 context("Test aic")
 
 test_that("aic() returns an aic score of a specific model, given observed y, predicted y and number of predictive variable", {
-  # expect outputs:
-  expect_equal(round(aic(c(1,2,3,4), c(5,6,7,8), 3), 3), 72)
-
+  # expected outputs:
+  expect_equal(round(aic(c(1,2,3,4), c(5,6,7,8), 3), 3), 17.09)
+  expect_equal(round(aic(c(-5,1,-8,-12), c(-4,-6,-8,1), 5), 3), 26.011)
+  expect_equal(round(aic(c(9.80, 105.23, 4.51, -65.75), c(9.81, 100.10, 4.51, -65.70), 10), 3), 27.536)
+  expect_equal(round(aic(c(0, 0, 0, -65.789), c(1, 0, 0.9, -64), 4), 3), 8.901)
 
   # expect_errors:
   ## test if y and y_pred have same length, if not yield error
