1a3ccbabc9c874cc40e2fe6c7de807f507a75ab6
Simran Sethi <simrannsethi@gmail.com>
2018-03-11 04:12:22 -0700

    added BIC function definition and updated vignette

--- R/bic.R
+++ R/bic.R
@@ -13,10 +13,77 @@
 #' 
 #' @details BIC = n X log(residual sum of squares/n) + K X log(n)
 #'
-#' @return float
+#' @return double
 #'
 #' @examples
 #' y <-c(1,2,3,4)
 #' y_pred <- c(5,6,7,8)
 #' p <- 3
 #' bic(y, y_pred, p)
+
+# library dependencies
+library(tidyverse)
+
+bic <- function(y, y_pred, p){
+  
+  ################## input type error exceptions ##################
+  
+  if(!is.vector(y)){
+    stop("Error: Argument 1 is not a vector")
+  }
+  
+  if(!is.vector(y_pred)){
+    stop("Error: Argument 2 is not a vector")
+  }
+  
+  for(i in y){
+    if(!is.double(i)) stop("Error: Elements of argument 1 must be double")
+  }
+  
+  for(i in y_pred){
+    if(!is.double(i)) stop("Error: Elements of argument 2 must be double")
+  }
+  
+  if(!is.double(p)) stop("Error: Number of variables must be a double")
+  
+  if(!(p%%1==0)) stop("Error: Number of variables must be a positive integer")
+  
+  if(p<=0) stop("Error: Number of variables must be a positive integer")
+  
+  if(length(y) <= 1) stop("Error: length of argument 1 cannot be less than or equal to 1")
+  
+  if(length(y_pred) <= 1) stop("Error: Length of argument 2 cannot be less than or eqaul to 1")
+  
+  ################## length of input vectors exceptions ##################
+  
+  if(length(y)!=length(y_pred)) stop("Error: Input vectors must be of equal lengths") else n <- length(y)
+    
+  ################## calculate score ##################
+  
+  # calculate residuals
+  residual <- y_pred - y
+  # calculate residual sum of square
+  RSS <- sum(residual^2)
+  # calculate BIC score
+  Bic <- n * log(RSS/n) + p * log(n)
+  
+  return(Bic)
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
